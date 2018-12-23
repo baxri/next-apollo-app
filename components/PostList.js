83e8581bd60da980ef283d787bafb1df8cf80dff
giorgi <giorgi.bibilashvili89@gmail.com>
2018-12-23 16:07:55 +0400

    --

--- components/PostList.js
+++ components/PostList.js
@@ -5,13 +5,23 @@ import { Query } from "react-apollo";
 export default class PostList extends Component {
   render() {
     return (
-      <Query query={POSTS} variables={{ count: 20 }}>
-        {({ loading, error, data }) => data.allPosts.map(post => (
-          <div>
-            <h3>{post.title}</h3>
-            <p>{post.body}</p>
-          </div>
-        ))}
+      <Query query={POSTS} variables={{ count: 2 }}>
+        {({ loading, error, data }) => {
+          if (loading) return "Loading...";
+          if (error) return `Error! ${error.message}`;
+
+          return (
+            <div>
+              {data.allPosts.map(post => (
+                <div key={post.id}>
+                  <h3>{post.title}</h3>
+                  <p>{post.body}</p>
+                </div>
+              ))}
+            </div>
+          );
+
+        }}
       </Query>
     )
   }

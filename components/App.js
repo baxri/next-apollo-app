ced4ba664857afd6f6475145e81fefb923416c88
giorgi <giorgi.bibilashvili89@gmail.com>
2018-12-31 19:53:07 +0400

    --

--- components/App.js
+++ components/App.js
@@ -26,6 +26,14 @@ export default class App extends Component {
     return (
       <div className="root">
         {children}
+        <style global jsx>{`
+
+            body{
+              margin: 0;
+              background-color: #f0f0f0;
+            }
+
+        `}</style>
       </div>
     )
   }

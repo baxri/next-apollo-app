14eb867b1457a6353f4c8cc2b8c7c8cdfa5f46c1
giorgi <giorgi.bibilashvili89@gmail.com>
2019-01-06 23:05:02 +0400

    Add pm2 process file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,6 @@
 module.exports = {
-  apps : [{
+  apps: [{
     name: "Next.js - Webmation",
     script: "npm run start",
-    env: {
-      NODE_ENV: "development",
-    },
-    env_production: {
-      NODE_ENV: "production",
-    }
   }]
 }

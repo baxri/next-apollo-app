022c5580cd7dc7578c4dce86a2ca78c8fb18fa16
giorgi <giorgi.bibilashvili89@gmail.com>
2019-01-11 21:04:25 +0400

    --

--- components/HeaderDropDownMenu.js
+++ components/HeaderDropDownMenu.js
@@ -8,23 +8,72 @@ export default class HeaderDropDownMenu extends Component {
         const { userid } = this.props
 
         return (
-            <div className="btn-group">
-                <button className="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Account
-                </button>
+            <div>
+                <div className="btn-group">
+                    <button className="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        @ Enhacements
+                    </button>
 
-                <div className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-
-                    <Link prefetch href='/profile'>
-                        <a className="dropdown-item" href="#">Profile page</a>
-                    </Link>
-
-                    <div className="logout-component">
-                        <Logout userid={userid} />
+                    <div className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                        <Link prefetch href='#'>
+                            <a className="dropdown-item" href="#">Printing Services</a>
+                        </Link>
+                        <Link prefetch href='#'>
+                            <a className="dropdown-item" href="#">Promitional Items</a>
+                        </Link>
+                        <Link prefetch href='#'>
+                            <a className="dropdown-item" href="#">Office 365 LIcenses</a>
+                        </Link>
+                        <Link prefetch href='#'>
+                            <a className="dropdown-item" href="#">Tehephone Integration</a>
+                        </Link>
+                        <Link prefetch href='#'>
+                            <a className="dropdown-item" href="#">Managed Marketing</a>
+                        </Link>
+                        <Link prefetch href='#'>
+                            <a className="dropdown-item" href="#">Aceppt Credit Cards & ACH</a>
+                        </Link>
                     </div>
-                </div>
 
-                <style jsx>{`
+                    <style jsx>{`
+                    .btn-group{
+                        margin-left: 7px;
+                    }
+                
+                `}</style>
+                </div>
+                <div className="btn-group">
+                    <button className="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Account
+                    </button>
+
+                    <div className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+
+                        <Link prefetch href='/profile'>
+                            <a className="dropdown-item" href="#">Profile Settings</a>
+                        </Link>
+                        <Link prefetch href='#'>
+                            <a className="dropdown-item" href="#">Company Settings</a>
+                        </Link>
+                        <Link prefetch href='#'>
+                            <a className="dropdown-item" href="#">Billing/Upgrade</a>
+                        </Link>
+                        <Link prefetch href='#'>
+                            <a className="dropdown-item" href="#">Rewards Center</a>
+                        </Link>
+                        <Link prefetch href='#'>
+                            <a className="dropdown-item" href="#">Help Center</a>
+                        </Link>
+                        <Link prefetch href='#'>
+                            <a className="dropdown-item" href="#">10000 Add Credits</a>
+                        </Link>
+
+                        <div className="logout-component">
+                            <Logout userid={userid} />
+                        </div>
+                    </div>
+
+                    <style jsx>{`
 
                     .logout-component{
                         padding-left: 25px;
@@ -38,6 +87,7 @@ export default class HeaderDropDownMenu extends Component {
                     }
                 
                 `}</style>
+                </div>
             </div>
         )
     }

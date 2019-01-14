8c0eaaffb378cd17d546837edbe5c832ca16739d
giorgi <giorgi.bibilashvili89@gmail.com>
2019-01-14 20:27:01 +0400

    --

--- pages/profile/components/ProfileDataForm.js
+++ pages/profile/components/ProfileDataForm.js
@@ -19,14 +19,37 @@ export default class componentName extends Component {
             required: ["email"],
             properties: {
                 // regitered_at: { type: "string", title: "Registered at" },
-                email: { type: "string", title: "Email" },
+                // email: { type: "string", title: "Email" },
                 first_name: { type: "string", title: "Firstname" },
+                middle_name: { type: "string", title: "Middle Name" },
                 last_name: { type: "string", title: "Lastname" },
-                status: {
-                    type: "string",
-                    enum: ["active", "inactive"],
-                    enumNames: ["Active", "inactive"]
-                },
+
+                prefix: { type: "string", title: "Prefix" },
+                suffix: { type: "string", title: "Suffix" },
+                initials: { type: "string", title: "Initials" },
+                birthday: { type: "string", title: "Birthday" },
+                title: { type: "string", title: "Title" },
+                signature: { type: "string", title: "Signature" },
+                home_phone: { type: "string", title: "Home Phone" },
+                work_phone: { type: "string", title: "Work Phone" },
+                ext: { type: "string", title: "Ext" },
+                direct_phone: { type: "string", title: "Direct Phone" },
+                mobile: { type: "string", title: "Mobile" },
+                fax: { type: "string", title: "Fax" },
+                
+                twitter: { type: "string", title: "Twitter" },
+                facebook: { type: "string", title: "Facebook" },
+                youtube: { type: "string", title: "Youtube" },
+                google: { type: "string", title: "Google" },
+                instagram: { type: "string", title: "Instagram" },
+                snapchat: { type: "string", title: "Snapchat" },
+                linkedin: { type: "string", title: "Linkedin" },
+
+                // status: {
+                //     type: "string",
+                //     enum: ["active", "inactive"],
+                //     enumNames: ["Active", "inactive"]
+                // },
                 // published: { type: "boolean", title: "Published", default: true },
             }
         }

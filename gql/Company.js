ae6a96369cee2f3c3cc9fed6f28bf720604660f0
giorgi <giorgi.bibilashvili89@gmail.com>
2019-01-15 13:35:50 +0400

    Integrate Fragments in webmation

--- gql/Company.js
+++ gql/Company.js
@@ -1,40 +1,48 @@
 import gql from "graphql-tag";
 
-export const COMPANY = gql`
-query{
-    company{
-          id
-          account_number_prefix
-          account_number_start
-          account_number_last
-          max_users
-  
-          name
-          address1
-          address2
-          city
-          state
-          postal
-          postal4
-          country
-  
-          phone
-          fax
-          email
-          website
-  
-          domain
-          domain_tld
-  
-          twitter
-          facebook
-          youtube
-          google
-          instagram
-          snapchat
-          linkedin
+export const CompanyFields = gql`
+    fragment CompanyFields on Company{
+        id
+        account_number_prefix
+        account_number_start
+        account_number_last
+        max_users
+
+        name
+        address1
+        address2
+        city
+        state
+        postal
+        postal4
+        country
+
+        phone
+        fax
+        email
+        website
+
+        domain
+        domain_tld
+
+        twitter
+        facebook
+        youtube
+        google
+        instagram
+        snapchat
+        linkedin
     }
-}`;
+`;
+
+export const COMPANY = gql`
+    query{
+        company{
+           ...CompanyFields 
+        }
+    }
+    ${CompanyFields}
+`;
 
 
 
--- gql/User.js
+++ gql/User.js
@@ -1,5 +1,43 @@
 import gql from "graphql-tag";
 
+export const UserFields = gql`
+    fragment UserFields on User {
+        id
+        first_name
+        last_name
+        middle_name
+
+        prefix
+        suffix
+        initials
+        birthday
+
+        title
+        signature
+
+        home_phone
+        work_phone
+        ext
+        direct_phone
+        mobile
+        fax
+
+        twitter
+        facebook
+        youtube
+        google
+        instagram
+        snapchat
+        linkedin
+
+        email
+        status
+        access_token
+        profile_url
+    }
+`;
+
+
 export const TOKEN = gql`
       mutation Token(
         $client_id: Int!, 
@@ -15,155 +53,35 @@ export const TOKEN = gql`
             password: $password
             client_secret: $client_secret
         ) {
-            id
-            first_name
-            last_name
-            middle_name
-
-            prefix
-            suffix
-            initials
-            birthday
-
-            title
-            signature
-
-            home_phone
-            work_phone
-            ext
-            direct_phone
-            mobile
-            fax
-
-            twitter
-            facebook
-            youtube
-            google
-            instagram
-            snapchat
-            linkedin
-
-            email
-            status
-            access_token
-            profile_url
+           ...UserFields
         }
     }
+    ${UserFields}
 `;
 
 export const LOGOUT = gql`
     mutation Logout($id: Int!){
          logout(id: $id) {
-            id
-            first_name
-            last_name
-            middle_name
-
-            prefix
-            suffix
-            initials
-            birthday
-
-            title
-            signature
-
-            home_phone
-            work_phone
-            ext
-            direct_phone
-            mobile
-            fax
-
-            twitter
-            facebook
-            youtube
-            google
-            instagram
-            snapchat
-            linkedin
-
-            email
-            status
-            access_token
-            profile_url
+            ...UserFields
         }
     }
+    ${UserFields}
 `;
 
 export const USER = gql`
    query{
         user {
-            id
-            first_name
-            last_name
-            middle_name
-
-            prefix
-            suffix
-            initials
-            birthday
-
-            title
-            signature
-
-            home_phone
-            work_phone
-            ext
-            direct_phone
-            mobile
-            fax
-
-            twitter
-            facebook
-            youtube
-            google
-            instagram
-            snapchat
-            linkedin
-
-            email
-            status
-            access_token
-            profile_url
+           ...UserFields
         }
     }
+    ${UserFields}
 `;
 
 export const UPDATE_USER = gql`
     mutation UpdateUser($id: Int!){
-            updateUser(id: $id) {
-                id
-            first_name
-            last_name
-            middle_name
-
-            prefix
-            suffix
-            initials
-            birthday
-
-            title
-            signature
-
-            home_phone
-            work_phone
-            ext
-            direct_phone
-            mobile
-            fax
-
-            twitter
-            facebook
-            youtube
-            google
-            instagram
-            snapchat
-            linkedin
-
-            email
-            status
-            access_token
-            profile_url
+        updateUser(id: $id) {
+            ...UserFields
         }
     }
+    ${UserFields}
 `;

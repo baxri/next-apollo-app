f6d3a3a77b0e4090dc67c91b8888e016f2ec9b13
giorgi <giorgi.bibilashvili89@gmail.com>
2019-01-20 17:12:52 +0400

    --

--- components/crud/Table.js
+++ components/crud/Table.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Query } from "react-apollo";
 import BootstrapTable from 'react-bootstrap-table-next';
+import Link from 'next/link'
 
 function priceFormatter(cell, row) {
 
@@ -34,15 +35,61 @@ export default class Table extends Component {
                     }, {
                         dataField: 'description',
                         text: 'Description'
-                    }, {
-                        dataField: '',
-                        text: '',
-                        formatter: priceFormatter
-                    }];
+                    },
+                        // {
+                        //     dataField: '',
+                        //     text: '',
+                        //     formatter: priceFormatter
+                        // }
+                    ];
 
                     return (<div>
 
-                        <BootstrapTable keyField='id' data={data.feeds} columns={columns} bootstrap4={true} />
+
+                        <div className="d-none d-md-block">
+                            <BootstrapTable keyField='id' data={data.feeds} columns={columns} bootstrap4={true} />
+                        </div>
+
+                        <div className="d-block d-md-none">
+                            {data.feeds.map((item, key) => {
+                                return (<div key={key} className="d-flex flex-column list-row">
+                                    {columns.map((col, ckey) => {
+                                        return (<div key={ckey} className="d-flex item-field">
+                                            <strong>{col['text']} :</strong>&nbsp;
+                                            <p>{item[col['dataField']]}</p>
+                                        </div>);
+                                    })}
+                                    <div className="d-flex controle-buttons">
+                                        <Link prefetch href='/users'>
+                                            <a href="#"><i className="far fa-edit fa-fw"></i> Edit</a>
+                                        </Link>
+                                        &nbsp;
+                                        <Link prefetch href='/users'>
+                                            <a href="#"><i className="far fa-eye-card fa-fw"></i> Show</a>
+                                        </Link>
+                                        &nbsp;
+                                        <Link prefetch href='/users'>
+                                            <a href="#"><i className="far fa-remove-card fa-fw"></i> Delete</a>
+                                        </Link>
+                                    </div>
+                                </div>)
+                            })}
+                        </div>
+
+                        <style jsx>{`
+
+                            .list-row{
+                                border-bottom: 1px dotted lightgray;
+                                margin-bottom: 15px;
+                                padding-bottom: 15px;
+                            }
+
+                            strong, p{
+                                font-size: 10pt; 
+                                line-height: 5px;
+                            }
+                        
+                        `}</style>
 
                         {/* {JSON.stringify(data)} */}
                         {/* <button onClick={() => fetchMore({

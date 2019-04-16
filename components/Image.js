e2dedfbecb2ff19cedd9720417dbbeca6a569d8a
giorgi <giorgi.bibilashvili89@gmail.com>
2019-04-16 23:45:31 +0400

    Main Website configuration

--- components/Image.js
+++ components/Image.js
@@ -1,11 +1,7 @@
 import React from 'react'
 
 export default ({ url }) => {
-
-    console.log(url)
-
     if (url.length == 0) {
-
         return (
             <img src="https://www.freeiconspng.com/uploads/no-image-icon-6.png" height="50" className="rounded" />
         )

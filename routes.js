9baf7cd364b85dfb6cc7cc60528f18bc42600308
giorgi <giorgi.bibilashvili89@gmail.com>
2019-04-26 20:54:13 +0400

    --

--- routes.js
+++ routes.js
@@ -16,3 +16,5 @@ routes.add({ name: 'websites-show', pattern: '/websites/:id/show', page: 'websit
 // .add('user', '/user/:id', 'profile')                // user   profile   /user/:id
 // .add('/:noname/:lang(en|es)/:wow+', 'complex')      // (none) complex   /:noname/:lang(en|es)/:wow+
 // .add({name: 'beta', pattern: '/v3', page: 'v3'})    // beta   v3        /v3
+
+
